c1b171da4f928fd8664da76915cd526a6c96d617
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-19 03:13:54 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // PayNest — Service Worker
-const CACHE_NAME = "paynest-pwa-v5-firebase-delete-20260819";
+const CACHE_NAME = "paynest-pwa-v6-install-fix-20260819";
 
 const APP_SHELL = [
   "./",
@@ -7,6 +7,8 @@ const APP_SHELL = [
   "./style.css",
   "./app.js",
   "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
   "./sw.js"
 ];
 
