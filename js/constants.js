b11cc2282d73876372bc90b17d528178d802f6e3
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-20 00:23:46 +0700

    Delete constants.js

--- js/constants.js
+++
@@ -1,4 +0,0 @@
-export const STORAGE_KEY = "paynest_v1_data";
-export const SCHEMA_VERSION = 1;
-export const PAYMENT_TYPES = Object.freeze({ DAILY:"daily", WEEKLY:"weekly", MONTHLY:"monthly" });
-export const CONTRACT_STATUS = Object.freeze({ ACTIVE:"active", PAID:"paid" });
