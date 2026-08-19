20adae999db5dccbe6709ae8c8091922ab6fd330
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-20 00:23:30 +0700

    Delete auth.js

--- js/auth.js
+++
@@ -1,6 +0,0 @@
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut,onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
-import { auth } from "./firebase.js";
-export const register=(email,password)=>createUserWithEmailAndPassword(auth,email,password);
-export const login=(email,password)=>signInWithEmailAndPassword(auth,email,password);
-export const logout=()=>signOut(auth);
-export const watchAuth=cb=>onAuthStateChanged(auth,cb);
