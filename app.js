3e9cce57fe5a871f6e0154a5201b62c5207c4042
eknutthasakpetluanoio-rgb <ek.nutthasak.petluanoio@gmail.com>
2026-08-21 10:00:43 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ let firebaseError = null;
 let firebaseAuthPromise = null;
 let auth = { currentUser: null };
 let db = null;
-let initializeApp, getAuth, onAuthStateChanged, signInWithEmailAndPassword, sendPasswordResetEmail;
+let initializeApp, getAuth, firebaseOnAuthStateChanged, signInWithEmailAndPassword, sendPasswordResetEmail;
 let createUserWithEmailAndPassword, signOut, getFirestore, doc, getDoc;
 let setDoc, onSnapshot, serverTimestamp;
 
